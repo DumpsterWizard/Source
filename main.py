2278c4cd8d8bc8cb329d9eb3fe6dec09daebadd9
Dumpster Wizard <tazdingodan@gmail.com>
2023-11-10 19:50:20 +0200

    1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello World!")
-
-print("Hello again, world!")
+length = int(input("Enter your length: "))
+width = int(input("Enter your width: "))
+area = length * width
+print(f"With width {width} and length {length}, your area is {area}!")
