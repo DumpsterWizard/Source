c248b9a9c2ee55d10ee6b6f3615140cd943f8ddd
Dumpster Wizard <tazdingodan@gmail.com>
2023-11-11 23:59:58 +0200

    1

---
+++ Kahoot_Answers1.py
@@ -0,0 +1,24 @@
+# 1 Функция print() не считывает данные, он только выводит данные
+# 2 Функция input() возвращает данные только типа str
+# 3 Print(4 * 5.0)  =  20.00
+# 4 Print(float("2" + "2.5")* 2) = 45.0
+# 5 print(5 % 2) = 1
+# 6 print(5 // 2) = 2
+# 7 print(float("6") * 2) = 12.0
+# 8 a = input("Enter name")   print(name)  = Error, надо - print(a)
+# 9 name = input("Enter name")  print("name")  = name, надо print(name)
+# 10 name = input("Enter name")  print("Hi" + name) = Hiname
+# 11 a = input("Enter number5")  print(a * 4) = 5555, потому что input возвращает в str
+# 12 a = input("Enter number") print(int(a) * 4) = 20, в инпуте 5 ввели.
+# 13 print(4 * str(7)) = 7777, print скобки не выводит
+# 14 print(bool(64)) = True, 0 - False, всё остальное - True
+# 15 print(6 - 4.0) = 2.0
+# 16 print(7//3) = 2
+# 17 print(float("10") + 4) = 14.0
+# 18 a = input("Enter number1")  print(a + str(5)) = 15
+# 19 a = input("Enter number")  print(int(a) / 3.0) = 4.0
+# 20 a = input("Enter nuber9") print(bool(a))  = True
+# 21 name = input("Enter name") print(name) = Nike
+# 22 name = input("Enter your name") print(nam, " pay attention") = Error , ошибка в названии переменной
+# 23 Какой вариант наименования переменной является корректным? _1x, #x1, 1x, x 1?  = _1x является единственно верным среди предстравленных. 
+# Нижнее подчеркивание - единственный спецсимвол с которого можно начинать название переменной.
