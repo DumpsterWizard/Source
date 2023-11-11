0400ba9d3ac23be9d94fe9712f8722b31b1607e1
Dumpster Wizard <tazdingodan@gmail.com>
2023-11-11 22:39:58 +0200

    1

---
+++ hackerrank_ArithAlgor.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+
+
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+# No rounding or formatting is necessary.
