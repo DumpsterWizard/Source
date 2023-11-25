3d2bad12d232357f848240fab1f0e84215c12cbe
Dumpster Wizard <tazdingodan@gmail.com>
2023-11-25 16:30:50 +0200

    1

--- my_shit/rock_paper_scissors.py
+++ my_shit/rock_paper_scissors.py
@@ -1,30 +1,100 @@
-import random
-
+from random import randint
 
 print("...ROCK...\n ...PAPER...\n ...SCISSORS...")
 
 
 
-player1 = input("Rock, Paper or Scissors, player 1? (r / p / s): ")
-print("NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!\n NO CHEATING!!!")
-player2 = input("Rock, Paper or Scissors, player 2? (r / p / s): ")
+# player1 = input("Rock, Paper or Scissors, player 1? (r / p / s): ")
+# print("NO CHEATING!!!\n" *20)
 
-if player1 == "p" and player2 == "r":
-    print("PLAYER ONE WON!")
-elif player1 == "s" and player2 == "p":
-    print("PLAYER ONE WON!")
-elif player1 == "r" and player2 == "s":
-    print("PLAYER ONE WON!")
+# player2 = input("Rock, Paper or Scissors, player 2? (r / p / s): ")
 
-elif player2 == "p" and player1 == "r":
-    print("PLAYER TWO WON!")
-elif player2 == "s" and player1 == "p":
-    print("PLAYER TWO WON!")
-elif player2 == "r" and player1 == "s":
-    print("PLAYER TWO WON!")
+# if player1 == "p" and player2 == "r":
+#     print("PLAYER ONE WON!")
+# elif player1 == "s" and player2 == "p":
+#     print("PLAYER ONE WON!")
+# elif player1 == "r" and player2 == "s":
+#     print("PLAYER ONE WON!")
 
-elif player1 == player2:
+# elif player2 == "p" and player1 == "r":
+#     print("PLAYER TWO WON!")
+# elif player2 == "s" and player1 == "p":
+#     print("PLAYER TWO WON!")
+# elif player2 == "r" and player1 == "s":
+#     print("PLAYER TWO WON!")
+
+
+# if player1 == "p":
+#     if player2 == "r":
+#         print("Player one wins!")
+# if player1 == "s":
+#     if player2 == "p":
+#         print("Player one wins!")
+# if player1 == "r":
+#     if player2 == "s":
+#         print("Player one wins!")
+
+# if player2 == "p":
+#     if player1 == "r":
+#         print("Player two wins!")
+# if player2 == "s":
+#     if player1 == "p":
+#         print("Player two wins!")
+# if player2 == "r":
+#     if player1 == "s":
+#         print("Player two wins!")
+
+# if player1 == player2:
+#     print("ITS A TIE! ROLL AGAIN!")
+
+# elif player1 == "p":
+#     if player2 == "r":
+#         print("Player one wins!")
+#     elif player2 == "s":
+#         print("Player two wins!")
+# if player1 == "s":
+#     if player2 == "r":
+#         print("Player two wins!")
+#     elif player2 == "p":
+#         print("Player one wins!")
+# if player1 == "r":
+#     if player2 == "s":
+#         print("Player one wins!")
+#     elif player2 == "p":
+#         print("Player two wins!")
+
+# else:
+#     print("You must enter r or p or s!") 
+
+
+player = input("Enter rock, paper or scissors (r / p / s): ")
+
+
+random_move = randint(0, 2)
+if random_move == 0:
+    computer = "r"
+elif random_move == 1:
+    computer = "s"
+else:
+    computer = "p"
+
+if player == computer:
     print("ITS A TIE! ROLL AGAIN!")
 
+if player == "r":
+    if computer == "s":
+        print("Player wins!")
+    elif computer == "p":
+        print("Computer wins!")
+if player == "s":
+    if computer == "r":
+        print("Computer wins!")
+    elif computer == "p":
+        print("Player wins!")
+if player == "p":
+    if computer == "s":
+        print("Computer wins!")
+    elif computer == "r":
+        print("Player wins!")
 else:
-    print("You must enter r or p or s!") 
+        print("Enter valid item!")
