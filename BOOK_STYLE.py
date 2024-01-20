c7ee9050cf8e4ac4a2857100c8db35db46d8e224
Dumpster Wizard <tazdingodan@gmail.com>
2024-01-20 19:13:06 +0200

    1

---
+++ BOOK_STYLE.py
@@ -0,0 +1,62 @@
+# # Программа подсчета слов!
+# line = input("Enter your text here, please!: ")
+# counted_line = line.count(" ") + 1
+# print(counted_line)
+
+# PI = 3.141592653589793
+# radius = int(input())
+# height = int(input())
+# volume = (PI * radius**2 * height) / 3
+# print(volume)
+
+# number = int(input())
+# print("sp" + ("o" * number) + "ky")
+
+# far = int(input()) * "far, "
+# print("A long time ago in a galaxy " + far.rstrip(", ") + " away...")
+
+# Y = int(input())
+# M = int(input())
+# print(M + (M - Y))
+
+# c = int(input())
+# print((c * 9 / 5) + 32)
+
+# paint = int(input())
+# paint_per_badge = int(input())
+# price_per_badge = int(input())
+
+# num_badges = paint // paint_per_badge  #calculate number of badges we can make
+# money_from_badges = num_badges * price_per_badge # Calculate money from selling badges
+# remaining_paint = paint % paint_per_badge # calculate remaining paint
+# money_from_rem_paint = remaining_paint # calculate money  from selling rem paint
+# total_money = money_from_badges + money_from_rem_paint # calculate total money we will make
+# print(total_money)
+
+# a3 = int(input())
+# a2 = int(input())
+# a1 = int(input())
+# a_total = (a3 * 3) + (a2 * 2) + a1
+
+# b3 = int(input())
+# b2 = int(input())
+# b1 = int(input())
+# b_total = (b3 * 3) + (b2 * 2) + b1
+
+# if a_total > b_total:
+#     print("A")
+# elif a_total < b_total:
+#     print("B")
+# else:
+#     print("T")
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# num4 = int(input())
+
+# if ((num1 == 8 or num1 == 9) and (num4 == 8 or num4 == 9) and (num2 == num3)):
+#     print("ignore")
+# else:
+#     print("answer")
+
---
+++ UDEMY.py
@@ -0,0 +1,83 @@
+# my_dict = {
+#     "username": "Dan",
+#     "age": "34",
+#     "powers": True,
+#     2: "Lol",
+#     "cock": 11,
+#     "brave": "Yes",
+#     "bold": "No",
+#     "balls": 2,
+#     "cars": ["lada", "sedan", "baklajan"]
+
+
+# }
+
+# my_dict2 = {
+#     "Nad": {
+#     "age": "43",
+#     "powers": False,
+#     2: "Rofl",
+#     "cock": 11,
+#     "brave": "No",
+#     "bold": "Yes",
+#     "balls": 3,
+#     "cars": ["Nogi", "huy", "basturma"]
+#     },
+#     "Frank": {
+#     "age": "34",
+#     "powers": True,
+#     2: "Lol",
+#     "cock": 11,
+#     "brave": "Yes",
+#     "bold": "No",
+#     "balls": 2,
+#     "cars": ["lada", "sedan", "baklajan"]
+#     },
+#     "Gogi": {
+#     "age": "34",
+#     "powers": True,
+#     2: "Lol",
+#     "cock": 11,
+#     "brave": "Yes",
+#     "bold": "No",
+#     "balls": 2,
+#     "cars": ["lada", "sedan", "baklajan"]
+#     }
+# }
+
+# # for name, userinfo in my_dict2.items():
+# #     print(f"The name is {name}")
+# #     brave = userinfo["brave"]
+# #     balls = userinfo["balls"]
+# #     print(f"{name} is brave? {brave}!")
+# #     print(f"And got {balls} balls of steel!\n")
+
+
+# print(my_dict2)
+# print(my_dict2.get("balls"))
+# print(my_dict2.get("cars", "brave"))
+
+
+# colors = ['red', 'green', 'yellow', 'purple', 'black', 'white']
+# index = 0
+# while index < len(colors):
+#     print(f"{index} >> {colors[index]}\n")
+#     index += 1
+
+# sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+# result = ""
+# for sound in sounds:
+#     result += sound
+# result = result.upper()
+# print(result)
+
+# Initially create an empty list called instructors
+# instructors = []
+# # Add the following strings to the instructors list 
+#     # "Colt"
+#     # "Blue"
+#     # "Lisa"
+# instructors = ["Colt", "Blue", "Lisa"]
+# # Run the tests to make sure you've done this correctly!
+# print(instructors)
+
